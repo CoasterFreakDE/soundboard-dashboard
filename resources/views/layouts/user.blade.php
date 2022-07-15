e9b01e42238c3d3eaabe1d753f6174d15ab1ceff
CoasterFreakDE <mleaked@gmail.com>
2022-07-15 12:05:46 +0200

    ⚓ Added sharelink support for all sounds on all pages

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -147,7 +147,65 @@
         </div>
 
 
+        <!-- Tooltip / Interaction menu -->
+        <menu type="context" id="soundmenu" class="hidden fixed top-[30px] left-[223px]">
+            <div class="mx-auto container max-w-[228px] bg-white rounded">
+                <nav class="space-y-1" aria-label="Actions">
+                    <a href="#" id="shareButton" class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 flex items-center px-3 py-2 text-sm font-medium rounded-md">
+                    <span class="truncate"><i class="fa-solid fa-share"></i> Share </span>
+                    </a>
+                </nav>
+
+                <svg class="absolute z-10  bottom-[-8px] " width="16" height="10" viewBox="0 0 16 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8 10L0 0L16 1.41326e-06L8 10Z" fill="white" />
+                </svg>
+            </div>
+        </menu>
+
+
+        
         <script>
+            const copyToClipboard = str => {
+            if (navigator && navigator.clipboard && navigator.clipboard.writeText)
+                return navigator.clipboard.writeText(str);
+                return Promise.reject('The Clipboard API is not available.');
+            };
+
+            document.addEventListener('contextmenu', event => {
+                // Test if event.path contains an li element with an id that starts with sound-
+                if (event.path.some(element => element.id && element.id.startsWith('sound-'))) {
+                    event.preventDefault();
+                    // Get element of event.path that starts with sound-
+                    const element = event.path.find(element => element.id && element.id.startsWith('sound-'));
+                    // Split id of element into array
+                    const id = element.id.split('-');
+                    // Get userid from array
+                    const userid = id[1];
+                    // Get sound name from array
+                    const sound = id[2];
+                    // Build url for sound
+                    const soundUrl = '{{ $soundLink }}/' + userid + '/' + sound;
+
+                    // Add the share link to the menu
+                    document.getElementById('shareButton').onclick = () => {
+                        copyToClipboard(soundUrl);
+                        showNotification('Copied to clipboard!', 'The share link has been copied to your clipboard.');	// Show notification
+                    };
+
+                    document.getElementById('soundmenu').style.top = (event.pageY - 40) + 'px';
+                    document.getElementById('soundmenu').style.left = event.pageX + 'px';
+                    document.getElementById('soundmenu').classList.remove('hidden');
+                } else {
+                    document.getElementById('soundmenu').classList.add('hidden');
+                }
+            });
+
+            document.addEventListener('click', event => {
+                if (event.target.id !== 'soundmenu') {
+                    document.getElementById('soundmenu').classList.add('hidden');
+                }
+            });
+
             function showNotification(title, message) {
                 document.getElementById('notification-title').innerText = title;
                 document.getElementById('notification-message').innerText = message;
