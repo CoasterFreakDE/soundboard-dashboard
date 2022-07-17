683c660e0e98bad74deeae70db26f3fd8b570395
Xanax <moritz.fuessel@flawcra.cc>
2022-07-17 18:30:05 +0200

    made navbar fixed

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -26,7 +26,7 @@
     </head>
 
     <body class="antialiased">
-    <nav class="bg-gradient-to-br from-[#017de8] to-[#4e9ee2]">
+    <nav class="fixed w-full bg-gradient-to-br from-[#017de8] to-[#4e9ee2]">
         <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div class="relative flex items-center justify-between h-16">
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
